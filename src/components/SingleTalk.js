4ad7fabe652cea2f643194accac7a46b83b638c0
nirkaufman <nirkaufman@gmail.com>
2019-08-08 13:01:16 -0400

    content

--- src/components/SingleTalk.js
+++ src/components/SingleTalk.js
@@ -7,7 +7,7 @@ const SingleTalk = ({talk}) => {
         <h3 className="mb-0">{talk.title}</h3>
         <div className="subheading mb-3">{talk.event} • {talk.location}</div>
         <p>{talk.abstract}</p>
-        <a href={talk.link}>Event website</a>
+        <a href={talk.link} target="_blank">Event website</a>
       </div>
       <div className="resume-date text-md-right">
         <span className="text-primary">{talk.date}</span>
