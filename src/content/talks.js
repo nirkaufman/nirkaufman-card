c0bf96fc6b6d8bdf62da2daea7d27d694f4969fc
nirkaufman <nirkaufman@gmail.com>
2019-08-14 13:33:33 -0400

    refactoring

--- src/content/talks.js
+++ src/content/talks.js
@@ -1,75 +1,99 @@
 export const talks = [
   {
-    title: "Angular & WordPress: Practical guide to headless CMS",
-    event: "International JavaScript Conference (IJS)",
-    location: "Munich, Germany",
-    date: "October 23, 2019",
+    title: '[MasterClass] React Hooks - A Deep Dive\n',
+    event: 'React & React Native - Israel',
+    location: 'Tel Aviv, Israel',
+    date: 'September, 26, 2019',
+    abstract: `Master React hooks API - From the inside-out.
+              We will start by building a small UI library from scratch, that implements a similar API to create components and hooks (A tiny React clone :).
+              We will use vanilla JavaScript, no dependencies.
+              Once we have done with his, we will explore React hooks in depth by a series of practical examples.             
+              By the end of this session, you will know everything about Hooks from the inside-out.
+`,
+    link: 'https://www.meetup.com/React-IL/events/263993642',
+  },
+  {
+    title: '[MasterClass] Modern State Management Patterns & Techniques In React',
+    event: 'ReactNYC',
+    location: 'New York City, New York, USA',
+    date: 'August 21, 2019',
+    abstract: `During this MasterClass, we will explore practical, real-world patterns and techniques for state management in React - both local and global.
+                By the end of this session, you will know how to use the new react-redux hooks API the right way, how to work with React built-in 
+                state management features - Context and component state - and how to choose the right pattern for the job.
+                `,
+    link: 'https://www.meetup.com/ReactNYC/events/263332786/',
+  },
+{
+    title: 'Angular & WordPress: Practical guide to headless CMS',
+    event: 'International JavaScript Conference (IJS)',
+    location: 'Munich, Germany',
+    date: 'October 23, 2019',
     abstract: `WordPress is still the most popular and powerful content management system with a massive, robust community behind it. In this talk, 
                I’ll show you how to use WordPress as a headless CMS, and build a modern and performant front-end for it using Angular.`,
-    link: "https://javascript-conference.com/angular/angular-wordpress-practical-guide-to-headless-cms/",
+    link: 'https://javascript-conference.com/angular/angular-wordpress-practical-guide-to-headless-cms/',
   },
 
   {
-    title: "Real-world advanced Redux patterns",
-    event: "International JavaScript Conference (IJS)",
-    location: "Munich, Germany",
-    date: "October 22, 2019",
+    title: 'Real-world advanced Redux patterns',
+    event: 'International JavaScript Conference (IJS)',
+    location: 'Munich, Germany',
+    date: 'October 22, 2019',
     abstract: `Redux is a modern, lean, front-end library for state management based on good old 
               messaging design patterns that have been used on the server side for many years. By choosing 
               it as your state management solution, you have to change your state of mind and start programming with 
               events. Through this session, we will learn practical advanced patterns and techniques for 
               solving real-world state management problems using vanilla JavaScript and help from Redux.`,
-    link: "https://javascript-conference.com/react/real-world-advanced-redux-patterns/",
+    link: 'https://javascript-conference.com/react/real-world-advanced-redux-patterns/',
   },
 
   {
-    title: "React Hooks MasterClass",
-    event: "ReactNYC",
-    location: "New York City, New York, USA",
-    date: "August 8, 2019",
-    abstract: "During this session, we will master the API and usage of hooks by implementing a lightweight version of React from scratch using nothing but vanilla JavaScript.",
-    link: "https://www.meetup.com/ReactNYC/events/263327927/",
+    title: 'React Hooks MasterClass',
+    event: 'ReactNYC',
+    location: 'New York City, New York, USA',
+    date: 'August 8, 2019',
+    abstract: 'During this session, we will master the API and usage of hooks by implementing a lightweight version of React from scratch using nothing but vanilla JavaScript.',
+    link: 'https://www.meetup.com/ReactNYC/events/263327927/',
   },
   {
-    title: "Refactoring with Uncle State",
-    event: "ReactNYC",
-    location: "New York City, New York, USA",
-    date: "August 15, 2019",
+    title: 'Refactoring with Uncle State',
+    event: 'ReactNYC',
+    location: 'New York City, New York, USA',
+    date: 'August 15, 2019',
     abstract: `Join your favorite Uncle for a refactoring session!
       In the process, you will learn how to leverage tools, libraries, patterns, and techniques for productivity and clean code.
       And he might have some presents for you!`,
-    link: "https://www.meetup.com/ReactNYC/events/263327927/",
+    link: 'https://www.meetup.com/ReactNYC/events/263327927/',
   },
   {
-    title: "Create your first Angular library",
-    event: "AngularNYC",
-    location: "New York City, New York, USA",
-    date: "August 20, 2019",
+    title: 'Create your first Angular library',
+    event: 'AngularNYC',
+    location: 'New York City, New York, USA',
+    date: 'August 20, 2019',
     abstract: `In this talk, I will g 
                library from scratch using the Angular CLI. You will learn how to set up a 
                project correctly, how to configure your library, and how to generate a sample project to test it`,
-    link: "https://www.meetup.com/AngularNYC/events/263176736/",
+    link: 'https://www.meetup.com/AngularNYC/events/263176736/',
   },
 
   {
-    title: "A practical guide for building modern PWA",
-    event: "GDG Rochester Devfest 2019",
-    location: "Rochester, New York, USA",
-    date: "September 7, 2019",
+    title: 'A practical guide for building modern PWA',
+    event: 'GDG Rochester Devfest 2019',
+    location: 'Rochester, New York, USA',
+    date: 'September 7, 2019',
     abstract: `In this session, we will dive into the technology, tools, patterns, 
               and techniques for building progressive web applications. By the end of this session, 
               you will know how to leverage modern web technologies that will take your user experience to the next step.`,
-    link: "https://www.explara.com/e/devfest",
+    link: 'https://www.explara.com/e/devfest',
   },
   {
-    title: "Refactoring with Uncle State",
-    event: "ReactNYC",
-    location: "New York City, New York, USA",
-    date: "August 28, 2019",
+    title: 'Refactoring with Uncle State',
+    event: 'ReactNYC',
+    location: 'New York City, New York, USA',
+    date: 'August 28, 2019',
     abstract: `Join your favorite Uncle for a refactoring session!
       In the process, you will learn how to leverage tools, libraries, patterns, and techniques for productivity and clean code.
       And he might have some presents for you!`,
-    link: "https://www.meetup.com/ReactNYC/events/262083713/",
+    link: 'https://www.meetup.com/ReactNYC/events/262083713/',
   },
 
 
